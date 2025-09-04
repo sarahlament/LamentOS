8299c4bd90f7657ae799590a6dc330477632884a
Sarah Lament <4612427+sarahlament@users.noreply.github.com>
2025-09-04 02:13:24 -0500

    updated flake.nix and some options

--- conf/user.nix
+++ conf/user.nix
@@ -2,26 +2,26 @@
 
 {
 	services = {
-		flatpak.enable = true;	# flatpak is useful for some things
+		flatpak.enable = true; # flatpak is useful for some things
 		pipewire = {
-			enable = true;	# audio is wanted, I hope...
-			pulse.enable = true;	# pulse backend for pipewire
+			enable = true; # audio is wanted, I hope...
+			pulse.enable = true; # pulse backend for pipewire
 			alsa.enable = true;	# alsa backend for pipewire
 			wireplumber.enable = true;	# wireplumber session manager for pipewire
 		};
 	};
 
-	programs.zsh.enable = true;	# let's enable my prefered shell
+	programs.zsh.enable = true; # let's enable my prefered shell
 
 	users.users.lament = {
-		isNormalUser = true;	# I am not a system user
-		shell = pkgs.zsh;	# actually set zsh as my shell
-		extraGroups = [ "wheel" "systemd-journal" ];	# sudo and journals
+		isNormalUser = true; # I am not a system user
+		shell = pkgs.zsh; # actually set zsh as my shell
+		extraGroups = [ "wheel" "systemd-journal" ]; # sudo and journals
 	};
 	programs.hyprland = {
-		enable = true;	# enable the hyprland window manager
-		withUWSM = true;	# let's use better integration with systemd
-		xwayland.enable = true;	# and let's use the xwayland backend for things that need it
+		enable = true; # enable the hyprland window manager
+		withUWSM = true; # let's use better integration with systemd
+		xwayland.enable = true; # and let's use the xwayland backend for things that need it
 	};
 
 	# Let's install some fonts
@@ -37,5 +37,4 @@
 		nerd-fonts.noto
 		nerd-fonts.jetbrains-mono
 	];
-
 }
