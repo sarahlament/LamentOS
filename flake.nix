a80a32fb7064f166fa9687c4c55f3b157393945a
Sarah Lament <4612427+sarahlament@users.noreply.github.com>
2025-09-07 16:36:51 -0500

    retabbed things (I prefer actual tabs) and fixed an alias

--- flake.nix
+++ flake.nix
@@ -1,39 +1,39 @@
 {
-    description = "LamentOS";
+	description = "LamentOS";
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; # let's use nix-unstable
-        lanzaboote = {
-            url = "github:nix-community/lanzaboote/v0.4.2"; # because this is for secureboot, let's keep it at a specific version
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        home-manager = {
-            url = "github:nix-community/home-manager"; # let's use the most recent commit
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-    };
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; # let's use nix-unstable
+		lanzaboote = {
+			url = "github:nix-community/lanzaboote/v0.4.2"; # because this is for secureboot, let's keep it at a specific version
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		home-manager = {
+			url = "github:nix-community/home-manager"; # let's use the most recent commit
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
 
-    outputs = { self, nixpkgs, lanzaboote, home-manager, ... }:
-    let
-        system = "x86_64-linux"; # define the system type here
-    in
-    {
-        nixosConfigurations.lamentOS = nixpkgs.lib.nixosSystem { # create our system config, named lamentOS
-            inherit system;
-            modules = [
-                lanzaboote.nixosModules.lanzaboote # lanzaboote for secureboot
-                home-manager.nixosModules.home-manager # home manager for, well, home manager
+	outputs = { self, nixpkgs, lanzaboote, home-manager, ... }:
+	let
+		system = "x86_64-linux"; # define the system type here
+	in
+	{
+		nixosConfigurations.lamentOS = nixpkgs.lib.nixosSystem { # create our system config, named lamentOS
+			inherit system;
+			modules = [
+				lanzaboote.nixosModules.lanzaboote # lanzaboote for secureboot
+				home-manager.nixosModules.home-manager # home manager for, well, home manager
 
-                ./conf/boot.nix # config related to boot: encryption, boot, mounts, kernel
-                ./conf/core.nix # system-level config: graphics, network, display manager
-                ./conf/user.nix # user-level config: audio, users, window managers
-          
-                {
-                    system.stateVersion = "25.11"; # DO NOT CHANGE THIS
-                    nix.settings.experimental-features = [ "nix-command" "flakes" ]; # we obviously want flakes
-                    home-manager.useGlobalPkgs = true; # since I want this integrated with the system, let's use the system packages as well
-                    home-manager.useUserPackages = true; # and let's treat packages here as user packages, not 'home manager' packages
-                    home-manager.users.lament = {
+				./conf/boot.nix # config related to boot: encryption, boot, mounts, kernel
+				./conf/core.nix # system-level config: graphics, network, display manager
+				./conf/user.nix # user-level config: audio, users, window managers
+				
+				{
+					system.stateVersion = "25.11"; # DO NOT CHANGE THIS
+					nix.settings.experimental-features = [ "nix-command" "flakes" ]; # we obviously want flakes
+					home-manager.useGlobalPkgs = true; # since I want this integrated with the system, let's use the system packages as well
+					home-manager.useUserPackages = true; # and let's treat packages here as user packages, not 'home manager' packages
+					home-manager.users.lament = {
 						imports = [
 							./conf/home-modules/user.nix # main user config
 							./conf/home-modules/shell.nix # shell config: zsh and terminal
@@ -43,8 +43,8 @@
 							./conf/home-modules/gemini.nix # let's try out gemini-cli
 						];
 					};
-                }
-            ];
-        };
-    };
+				}
+			];
+		};
+	};
 }
